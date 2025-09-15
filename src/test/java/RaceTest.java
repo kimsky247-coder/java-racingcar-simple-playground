7c1319de414a32a64bc0965ab284f8ea0edb2cbf
kimsky247-coder <kimsky247@gmail.com>
2025-09-15 11:54:00 +0900

    RaceTest 가독성 및 구조 개선

--- src/test/java/RaceTest.java
+++ src/test/java/RaceTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,29 +9,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("Race 클래스 테스트")
 public class RaceTest {
+    private Race race;
+    private Car car1, car2, car3;
 
-    @Test
-    void 우승자가_한_명인_경우() {
-        Random random = new Random();
-        MoveCar moveCar = new MoveCar(random);
-        Race race =  new Race(moveCar);
+    @BeforeEach
+    void setUp() {
+        MoveCar moveCar = new MoveCar(new Random());
+        race = new Race(moveCar);
 
-        Car car1 = new Car("car1");
-        Car car2 = new Car("car2");
-        Car car3 = new Car("car3");
+        car1 = new Car("car1");
+        car2 = new Car("car2");
+        car3 = new Car("car3");
 
         race.addCar(car1);
         race.addCar(car2);
         race.addCar(car3);
+    }
 
-        car1.moveForward();
-
-        car2.moveForward();
-        car2.moveForward();
-
-        car3.moveForward();
-        car3.moveForward();
-        car3.moveForward();
+    @Test
+    void 우승자가_한_명인_경우_우승자_한_명만_반환한다() {
+        moveCarNTimes(car1, 1);
+        moveCarNTimes(car2, 2);
+        moveCarNTimes(car3, 3);
 
         List<String> winners = race.getWinners();
 
@@ -39,32 +39,22 @@ public class RaceTest {
     }
 
     @Test
-    void 우승자가_두_명_이상인_경우() {
-        Random random = new Random();
-        MoveCar moveCar = new MoveCar(random);
-        Race race =  new Race(moveCar);
-
-        Car car1 = new Car("car1");
-        Car car2 = new Car("car2");
-        Car car3 = new Car("car3");
-
-        race.addCar(car1);
-        race.addCar(car2);
-        race.addCar(car3);
-
-        car1.moveForward();
-
-        car2.moveForward();
-        car2.moveForward();
-
-        car3.moveForward();
-        car3.moveForward();
+    void 우승자가_두_명_이상인_경우_모든_우승자를_반환한다() {
+        moveCarNTimes(car1, 1);
+        moveCarNTimes(car2, 2);
+        moveCarNTimes(car3, 2);
 
         List<String> winners = race.getWinners();
 
         assertEquals(2, winners.size());
         assertEquals("car2", winners.get(0));
         assertEquals("car3", winners.get(1));
+    }
+
+    private void moveCarNTimes(Car car, int times) {
+        for (int i = 0; i < times; i++) {
+            car.moveForward();
+        }
     }
 
 }
